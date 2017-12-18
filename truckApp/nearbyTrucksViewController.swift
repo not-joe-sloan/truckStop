2dcbb31ab393e1414ba9f4b5784e6692f240021e
Joe Sloan <joe@joesloandesign.com>
2017-12-17 18:30:23 -0700

    Adds a nearby trucks page.

---
+++ truckApp/nearbyTrucksViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  truckApp
+//
+//  Created by Joe Sloan on 12/17/17.
+//  Copyright © 2017 Joe Sloan. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import Parse
+import CoreLocation
+
+class nearbyTrucksViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate, UITableViewDelegate, UITableViewDataSource {
+
+    //Outlets
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var trucksTable: UITableView!
+    
+    //Variables
+    var locationManager = CLLocationManager()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //SET UP THE LOCATION MANAGER
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+        
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "Cello, world."
+        return cell
+    }
+
+
+}
+
